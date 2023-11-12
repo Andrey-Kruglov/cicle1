5850f345c8e64fd7d3f30cb1172fe9ea89c0f507
boss <ovpvoap@mail.ru>
2023-11-12 20:03:23 +0300

    1commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 2");
+         for (int i =10; i >= 1; i = i - 1){
+             System.out.println(i);}
+        System.out.println("Задача 3");
+         for ()
+    }
+}
